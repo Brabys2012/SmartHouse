c18d98b74f7f965047d928e7bf04786ed9d6c975
Соколов <sokolov2211@gmail.com>
2012-12-01 19:30:11 +0400

    Добавлено:
    -Диаграмма классов контролера
    -Новое описание контролера
    -Дописан терминал
    -Изменен виртуальный контролер с соответствием с новым описанием контролера

---
+++ Software/Smart House/ControlsNOBD/Controls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControlsNOBD
+{
+    static public class Controls1
+    {
+        static public Parss_and_deter_mess PaDm = new Parss_and_deter_mess();
+        static public Datchik DatTest = new Datchik();
+        
+    }
+}
