f2b70ab407cd014add405dafd752a67e298be7e2
Вакин Дмитрий <Dmitry.Vakin.ID@gmail.com>
2013-01-27 16:08:53 +0400

    Клиент и сервер, для отладки

---
+++ Software/Сервер и Клиент/TCPServerOnSoket/TCPServerOnSoket/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порт для прослушки:");
+            AsynchronousIoServer ts = new AsynchronousIoServer(Convert.ToInt32(Console.ReadLine()));
+            ts.Start();
+            Console.WriteLine("Ожидание клиентов:");
+            Console.ReadLine();
+        }
+    }
+}
